1309dd9b6036d102ed2f2c055168066c57132f22
Abhinav Anand <abhinav011501@gmail.com>
2024-08-02 18:40:38 +0530

    Automated commit at 02-08-2024 18:40:38.79

---
+++ git_push.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd /d C:\Users\Abhinav\Documents\TechGreenWave\PMS\z_learning\hitesh_chai_backend_professional_project
+
+git add .
+
+git commit -m "Automated commit at %DATE% %TIME%"
+
+git push origin main
+
+endlocal
